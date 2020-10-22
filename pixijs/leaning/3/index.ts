986809e8a42cab7122afb472d60bc12de79caa78
XXL <xurishi@gmail.com>
2020-10-22 12:59:29 +0800

    pixijs leaning: 3. 创建 pixi 应用和舞台

---
+++ pixijs/leaning/3/index.ts
@@ -0,0 +1,25 @@
+/// <reference path="../../node_modules/@types/pixi.js/index.d.ts"/>
+
+const app = new PIXI.Application({
+  width: 256,
+  height: 256,
+  antialias: true,
+  transparent: false,
+  resolution: 1,
+  forceCanvas: true,
+});
+
+// (app.renderer as PIXI.CanvasRenderer)._backGroundColor = 0x061639;
+
+app.renderer.autoResize = true;
+app.renderer.resize(512, 512);
+
+// 整个窗口
+const { style } = app.renderer.view;
+style.position = 'absolute';
+style.display = 'block';
+app.renderer.autoResize = true;
+app.renderer.resize(window.innerWidth, window.innerHeight);
+
+
+document.body.appendChild(app.view);
--- pixijs/leaning/note.md
+++ pixijs/leaning/note.md
@@ -5,3 +5,18 @@
 # 2. 安装
 
 环境: pixi.js v4.5.5
+
+# 3. 创建 Pixi 应用和舞台
+
+`PIXI.Application`算出了应该使用 Canvas 还是 WebGL 去渲染图像, 它取决于你正在使用的浏览器支持哪一个.
+
+```js
+const app = PIXI.Application({
+    width: 256,
+    height: 256,
+    antialias: true,
+    transparent: false,
+    resolution: 1
+});
+```
+

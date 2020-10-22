9a6a74a3f6d1204a04da0da09f17d89c24fe19cd
XXL <xurishi@gmail.com>
2020-10-22 15:46:24 +0800

    pixijs examples: container transparent background

--- pixijs/examples/webpack.config.js
+++ pixijs/examples/webpack.config.js
@@ -30,26 +30,40 @@ const { options } = webpack({
   devServer: {
     open: true,
     port: 9000,
-    contentBase: __dirname,
+    contentBase: path.resolve(__dirname, 'dist'),
   },
 });
 
-fs.readdirSync(__dirname).forEach((file) => {
+const links = [];
+
+fs.readdirSync(path.join(__dirname, 'src')).forEach((file) => {
   if (file.endsWith('.ts')) {
     const lastIndex = file.lastIndexOf('.ts');
     // entry[file.substr(0, lastIndex)] = path.join(__dirname, file);
     const name = file.substr(0, lastIndex);
     configList.push({
       ...options, //
-      entry: path.join(__dirname, file),
+      entry: path.join(__dirname, 'src', file),
       output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist', name),
       },
     });
+    links.push(`./${name}/index.html`);
   }
 });
 // console.log(entry);
+
+fs.writeFile(
+  path.join(__dirname, 'dist', 'index.html'),
+  `
+<html></html>
+<body>${links
+    .map((link) => '<a href="' + link + '" >' + link + '</a>')
+    .join('<br />')}</body>
+`,
+  () => {}
+);
 
 // console.log(configList);
 

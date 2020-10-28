7e5ee50aa7de34d008375a57ba64ba2cbd992a55
XXL <xurishi@gmail.com>
2020-10-28 12:59:35 +0800

    INIT

---
+++ reduxsaga/src/router.js
@@ -0,0 +1,13 @@
+import { Route, Switch } from 'react-router-dom';
+
+export default () => (
+  <div>
+    <div>
+      <h1>Link</h1>
+    </div>
+    <div>
+      <Switch>
+      </Switch>
+    </div>
+  </div>
+);

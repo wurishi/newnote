54bc5f1687f2174aca58e977a11bd8c9c3c33ca1
XXL <xurishi@gmail.com>
2020-12-07 10:41:03 +0800

    123

--- leetcode/src/123.ts
+++ leetcode/src/123.ts
@@ -1,20 +1,20 @@
-export function maxProfit(prices: number[]): number {
-  const n = prices.length;
-  const max_k = 2;
-  const dp: number[][][] = [];
-  for (let i = 0; i < n; i++) {
-    if (!dp[i]) {
-      dp[i] = [];
-    }
-    for (let k = max_k; k >= 1; k--) {
-      if (!dp[i][k]) {
-        dp[i][k] = [];
-      }
-      if (i - 1 == -1) {
-        dp[i][k][0] = 0;
-        dp[i][k][1] = -prices[i];
-      }
-      // dp[i][k][0] =
-    }
-  }
-}
+// export function maxProfit(prices: number[]): number {
+//   const n = prices.length;
+//   const max_k = 2;
+//   const dp: number[][][] = [];
+//   for (let i = 0; i < n; i++) {
+//     if (!dp[i]) {
+//       dp[i] = [];
+//     }
+//     for (let k = max_k; k >= 1; k--) {
+//       if (!dp[i][k]) {
+//         dp[i][k] = [];
+//       }
+//       if (i - 1 == -1) {
+//         dp[i][k][0] = 0;
+//         dp[i][k][1] = -prices[i];
+//       }
+//       // dp[i][k][0] =
+//     }
+//   }
+// }

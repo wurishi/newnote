ebdf07fbc38625bb1b62eb901e62f34bb944bd02
XXL <xurishi@gmail.com>
2021-03-04 14:03:02 +0800

    多种效果

--- threejs/webglfundamentals/webgl-utils.ts
+++ threejs/webglfundamentals/webgl-utils.ts
@@ -177,3 +177,55 @@ export function createCanvas(): HTMLCanvasElement {
   document.body.appendChild(canvas);
   return canvas;
 }
+
+export function setRectangle(
+  gl: WebGLRenderingContext,
+  target: number,
+  x: number,
+  y: number,
+  width: number,
+  height: number
+) {
+  const x1 = x,
+    x2 = x + width;
+  const y1 = y,
+    y2 = y + height;
+  gl.bufferData(
+    target,
+    new Float32Array([x1, y1, x2, y1, x1, y2, x1, y2, x2, y1, x2, y2]),
+    gl.STATIC_DRAW
+  );
+}
+
+export function setTexcoord(gl: WebGLRenderingContext, target: number) {
+  gl.bufferData(
+    target,
+    new Float32Array([
+      0.0,
+      0.0,
+      1.0,
+      0.0,
+      0.0,
+      1.0,
+      0.0,
+      1.0,
+      1.0,
+      0.0,
+      1.0,
+      1.0,
+    ]),
+    gl.STATIC_DRAW
+  );
+}
+
+export function createAndSetupTexture(gl: WebGLRenderingContext) {
+  const texture = gl.createTexture();
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+
+  return texture;
+}

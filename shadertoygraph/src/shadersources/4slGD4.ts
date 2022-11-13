0cbc1d9420818d4635e9039ddf1f015e86926421
xuxiaolei <xurishi@gmail.com>
2022-11-13 13:43:11 +0800

    shadertoy

---
+++ shadertoygraph/src/shadersources/4slGD4.ts
@@ -0,0 +1,14 @@
+import { Config } from '../type';
+import fragment from './glsl/4slGD4.glsl?raw';
+export default [
+  {
+    // '4slGD4': 'Mountains',
+    name: '4slGD4',
+    type: 'image',
+    fragment,
+    channels: [
+      { type: 'music' },
+      { type: 'texture', src: 'Pebbles', filter: 'mipmap', wrap: 'repeat' },
+    ],
+  },
+] as Config[];

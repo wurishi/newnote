a139defd43b15481386f8dae5813e1f568d60212
xiaolexu <xiaolexu@paypal.com>
2022-11-14 09:50:03 +0800

    shadertoy

---
+++ shadertoygraph/src/shadersources/4ds3zn.ts
@@ -0,0 +1,11 @@
+import { Config } from '../type'
+import fragment from './glsl/4ds3zn.glsl?raw'
+export default [
+    {
+        // '4ds3zn': 'Apollonian',
+        name: '4ds3zn',
+        type: 'image',
+        fragment,
+        channels: [],
+    },
+] as Config[]

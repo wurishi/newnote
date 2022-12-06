9cac1d4413f990619b48814ee718af4ed6369783
xiaolexu <xiaolexu@paypal.com>
2022-12-06 10:04:13 +0800

    deckgl

--- deckgl/src/App.tsx
+++ deckgl/src/App.tsx
@@ -2,11 +2,12 @@ import React, { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import ArcLayer from './layers/arc-layer'
+import ContourLayer from './layers/contour-layer'
 
 function App() {
     return (
         <div className="App">
-            <ArcLayer />
+            <ContourLayer />
         </div>
     )
 }
--- deckgl/src/deck.d.ts
+++ deckgl/src/deck.d.ts
@@ -1,3 +1,5 @@
 declare module '@deck.gl/react';
 
 declare module '@deck.gl/layers';
+
+declare module '@deck.gl/aggregation-layers';

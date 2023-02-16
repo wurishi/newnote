f28026b9e18af67981ec0bb44c31c8a0cb0e0d06
xiaolexu <xiaolexu@paypal.com>
2023-02-16 19:07:49 +0800

    md

--- threejsexample/public/note.md
+++ threejsexample/public/note.md
@@ -1,71 +1,86 @@
-first
+# 1. WebGLRenderer
 
-```
-console.log(100)
-```
+渲染器
 
-end
+- `new WebGLRenderer({ antialias })`:
+  - `antialias?: boolean`: 设置防锯齿，默认为 false
 
-```
-```
+- `.setPixelRatio(value)`: 设置渲染器像素比例
+  - `value: number`
 
-empty
+- `.setSize(width, height, updateStyle)`: 设置渲染尺寸
+  - `width: number`
+  - `height: number`
+  - `updateStyle?: boolean`: 是否随着尺寸的变化而更新画面的样式（style），默认为 true
 
-# 01. 从一个对象生成一个联合类型
+- `.outputEncoding: TextureEncoding`: 设置渲染的输出格式
 
-```typescript
-export const fruitCounts = {
-  apple: 1,
-  pear: 4,
-  banana: 26,
-};
+# 2. Scene
 
-type SingleFruitCount =
-  | { apple: number }
-  | { banana: number }
-  | { pear: number };
+- `.environment`: 若该值不为 null，则该纹理贴图将会被设为场景中所有物理材质的环境贴图。该属性不能覆盖已经存在并分配给 `MeshStandardMaterial.envMap` 的贴图。默认为 null
 
-const singleFruitCount: SingleFruitCount = {
-  banana: 12,
-};
-```
+- `.background: Color | Texture`: 设置背景色，默认为 null
 
-看起来不错
+# 3. PerspectiveCamera
 
-### Browser
+- `new PerspectiveCamera(fov, aspect, near, far)`:
+  - `fov?: number`: 50
+  - `aspect?: number`: 1
+  - `near?: number`: 0.1
+  - `far?: number`: 2000
 
-It also works in a browser; here is a complete example:
+# 101. PMREMGenerator
 
-```html
-<!DOCTYPE html>
-<html>
-  <body>
-    <textarea id="text-input" oninput="this.editor.update()"
-              rows="6" cols="60">Type **Markdown** here.</textarea>
-    <div id="preview"> </div>
-    <script src="lib/markdown.js"></script>
-    <script>
-      function Editor(input, preview) {
-        this.update = function () {
-          preview.innerHTML = markdown.toHTML(input.value);
-        };
-        input.editor = this;
-        this.update();
-      }
-      var $ = function (id) { return document.getElementById(id); };
-      new Editor($("text-input"), $("preview"));
-    </script>
-  </body>
-</html>
-```
+Mipmapped Radiance Environment Map(PMREM)。
 
-### Command Line
+该类可以从一个 CubeMap 环境纹理中生成一个预过滤的环境纹理。通过这种方式计算出来的环境贴图，拥有一个平稳的漫反射照明，并被限制了采样计算。
 
-Assuming you've installed the `md2html` script (see Installation,
-above), you can convert Markdown to HTML:
+- `new PMREMGenerator(renderer)`:
+  - `renderer: THREE.WebGLRenderer`
 
-```javascript
-for(const item in object) {
-  
-}
-```
+- `.fromScene(scene, sigma, near, far): WebGLRenderTarget`:
+  - `scene: THREE.Scene`: 指定场景
+  - `sigma?: number`: 指定一个以弧度为单位的模糊半径。默认值为 0
+  - `near?: number`: number 近平面值，默认值为 0.1
+  - `far?: number`: number 远平面值，默认值为 100
+  - `return WebGLRenderTarget`: 返回一个渲染缓冲，GPU 会在后台将场景中的内容渲染到缓冲中
+    - `.texture: Texture`: 该纹理保存了渲染缓冲中的像素，可以作为后续进一步处理的输入值
+
+# 102. OrbitControls
+
+轨道控制器
+
+- `.enablePan`: 设置为 false 时，控制器将不再接受平移操作（鼠标右键拖动）。默认值为 true
+
+- `.enableDamping`: 移动阻尼，默认值为 false
+
+# 103. GLTFLoader
+
+glb 模型格式加载器
+
+- `.setDRACOLoder(dracoLoader)`: 设置解压器
+
+- `.load(url, onLoad, onProgress, onError)`: 加载模型
+  - `url: string`: 模型路径
+  - `onLoad: (gltf:GLTF) => void`: 加载成功
+    - `gltf.scene`: 主场景
+    - `gltf.scenes`: 所有场景
+    - `gltf.animations`: 所有动画  
+  - `onProgress?: (event: ProgressEvent<EventTarget>) => void`: 加载进度
+  - `onError?: (event: ErrorEvent) => void`: 加载失败
+
+# 104. DRACOLoader
+
+glb 模型的解压加载器，一个 `wasm` 的解码器。
+
+- `.setDecoderPath(path)`: 指定 `.wasm` 和 `.js` 文件的路径。
+
+# 105. AnimationMixer
+
+动画混合器
+
+- `new AnimationMixer(model)`: 
+  - `model: Object3D | AnimationObjectGroup`: 包含了一个或多个动画剪辑信息的模型
+
+- `.clipAction(clip, root, blendMode): AnimationAction`: 剪辑动画
+  - `clip: AnimationClip`: 动画剪辑
--- threejsexample/src/main.ts
+++ threejsexample/src/main.ts
@@ -1,4 +1,5 @@
 import './style.css'
+import 'highlight.js/styles/vs2015.css' // dark, github, default
 
 import Stats from 'stats.js'
 
@@ -19,12 +20,30 @@ mainLoop()
 
 import { GUI } from 'dat.gui'
 import Example from './libs/example'
+import { createPreview, loadMarkdown } from './libs/md'
 
 const menu = new GUI()
 ui.appendChild(menu.domElement.parentElement!)
 menu.domElement.setAttribute('style', 'float: left; margin-top: 48px;')
 
-menu.addFolder('菜单')
+const preview = createPreview(ui)
+preview.setAttribute('style', 'display: none;')
+const previewCfg = {
+  enable: false,
+}
+menu
+  .add(previewCfg, 'enable')
+  .name('查看MD')
+  .onChange((v) => {
+    if (v) {
+      preview.setAttribute('style', 'display: block;')
+    } else {
+      preview.setAttribute('style', 'display: none;')
+    }
+  })
+
+const mUI = menu.addFolder('菜单')
+mUI.open()
 
 const menuSettings: any = {}
 const folderMap: Record<string, GUI> = {}
@@ -41,7 +60,7 @@ function buildMenu(examples: Record<string, () => Promise<unknown>>) {
     for (let i = 1; i < len; i++) {
       if (i !== len - 1) {
         // path
-        const parentFolder = folderMap[folderKey] || menu
+        const parentFolder = folderMap[folderKey] || mUI
         folderKey += pathList[i] + '/'
         let folder = folderMap[folderKey]
         if (!folder) {
@@ -53,7 +72,7 @@ function buildMenu(examples: Record<string, () => Promise<unknown>>) {
         // file
         const parentFolder = folderMap[folderKey]
         menuSettings[path] = () => {
-          startExample(examples[path])
+          startExample(path, examples[path])
           saveLoc(path)
         }
         parentFolder
@@ -70,7 +89,7 @@ menu.open()
 
 let currentExample: Example | null
 let currentExampleDestroy: (() => void) | null
-async function startExample(fn: () => Promise<unknown>) {
+async function startExample(path: string, fn: () => Promise<unknown>) {
   if (currentExample) {
     currentExampleDestroy && currentExampleDestroy()
     currentExampleDestroy = null
@@ -80,6 +99,8 @@ async function startExample(fn: () => Promise<unknown>) {
   const ExampleClass = module.default
   currentExample = new ExampleClass()
   currentExampleDestroy = currentExample!.startRun({ ui, container: app })
+
+  loadMarkdown(path)
 }
 
 function saveLoc(path: string) {
@@ -92,7 +113,7 @@ function loadLoc() {
     ;[lv3Examples, lv4Examples, lv5Examples].find((examples) => {
       const fn = examples[path]
       if (!!fn) {
-        startExample(fn)
+        startExample(path, fn)
         return true
       }
     })
@@ -106,98 +127,3 @@ const run = () => {
 run()
 
 loadLoc()
-
-import { markdown } from 'markdown'
-import hljs from 'highlight.js'
-import 'highlight.js/styles/vs2015.css' // dark, github, default
-
-function createPreprocessTreeNode() {
-  let idx = 0
-  let codeStart = false
-  let codeType = ''
-  let codeArray: string[] = []
-  const replaceList = new Array<{ name: string; code: string; type: string }>()
-  const randomName = () => {
-    return '###replace_' + Math.random() + '_' + idx++
-  }
-  return {
-    replaceCode: (html: string) => {
-      replaceList.forEach((item) => {
-        html = html.replace(
-          item.name,
-          `<pre><code class="language-${item.type}">${item.code}</code></pre>`
-        )
-      })
-      return html
-    },
-    preprocessTreeNode: (tree: any) => {
-      if (Array.isArray(tree) && tree[0] === 'para') {
-        if (codeStart) {
-          if (tree[1]) {
-            codeArray = codeArray.concat((tree[1] + '').split('\n'))
-          }
-          if (tree[2] && tree[2][0] === 'inlinecode') {
-            codeStart = false
-            const code = hljs.highlight(codeArray.join('\n'), {
-              language: codeType || 'html',
-            })
-            const name = randomName()
-            replaceList.push({
-              name,
-              code: code.value,
-              type: codeType || 'html',
-            })
-            return name
-          }
-          return ''
-        } else if (Array.isArray(tree[1]) && tree[1][0] === 'inlinecode') {
-          if (tree[1][1]) {
-            // 单行 code
-            codeArray = (tree[1][1] + '').split('\n')
-            codeType = codeArray.splice(0, 1).join('')
-            const code = hljs.highlight(codeArray.join('\n'), {
-              language: codeType || 'html',
-            })
-            const name = randomName()
-            replaceList.push({
-              name,
-              code: code.value,
-              type: codeType || 'html',
-            })
-            return name
-          } else if (tree[2]) {
-            codeArray = (tree[2] + '').split('\n')
-            codeType = codeArray.splice(0, 1).join('')
-            if (codeType[0] === '`') {
-              codeType = codeType.substring(1)
-            }
-            codeStart = true
-            return ''
-          }
-        }
-      }
-      return tree
-    },
-  }
-}
-
-fetch('/note.md').then((resp) => {
-  resp.text().then((raw) => {
-    const tn = createPreprocessTreeNode()
-    const tree = markdown.parse(raw)
-    const toTree = markdown.toHTMLTree(
-      tree,
-      {},
-      {
-        preprocessTreeNode: tn.preprocessTreeNode,
-      }
-    )
-    const html = markdown.renderJsonML(toTree)
-    const div = document.createElement('div')
-    div.innerHTML = tn.replaceCode(html)
-    document.body.appendChild(div)
-    console.log(tree)
-    // console.log(toTree)
-    // console.log(html)
-  })
-})

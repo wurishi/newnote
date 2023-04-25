556e2217460b33c85efc5fb15624603bb7a6db82
xxl <xurishi@gmail.com>
2023-04-25 18:23:28 +0800

    st code

--- shadertoygraph/src/parseglsl/index.ts
+++ shadertoygraph/src/parseglsl/index.ts
@@ -9,11 +9,11 @@ export default function (updateConfig: any, config: any, gui: GUI, passIndex: nu
     const pass: {
         code: string;
     } = config.renderpass[passIndex];
-    groupGLSL(pass.code);
+    graphList = groupGLSL(pass.code);
 
     const uiData = {
         code() {
-
+            createCodePanel(graphList);
         }
     };
     gui.add(uiData, 'code');
@@ -23,5 +23,25 @@ function groupGLSL(code: string) {
     const arr = code.split('\n')
     const graphArr = arr.map((code, index) => new GraphBasic(index, code));
 
-    graphList = graphArr;
+    return graphArr;
+}
+
+function createCodePanel(list: GraphBasic[]) {
+    const dom: HTMLDivElement = document.querySelector('#tools')!;
+
+    let panel: HTMLDivElement | null = document.querySelector('#codePanel');
+    if (!panel) {
+        panel = document.createElement('div');
+        panel.id = 'codePanel';
+        dom.appendChild(panel);
+    }
+
+    list.forEach(it => {
+        const item = document.createElement('div');
+        item.innerHTML = it.getCode();
+        // item.contentEditable = 'true';
+        // item.contentEditable = 'plaintext-only';
+        item.style.setProperty('-webkit-user-modify', 'read-write-plaintext-only');
+        panel!.appendChild(item);
+    })
 }
